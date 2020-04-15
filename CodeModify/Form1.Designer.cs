b94db5adc1374b061f4d1607afab9ff5836239ad
GuoWenxin <290729435@qq.com>
2020-04-15 15:20:23 +0800

    create a version

---
+++ CodeModify/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace CodeModify
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbFolder = new System.Windows.Forms.TextBox();
+            this.btnSelevtFolder = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbsuffix = new System.Windows.Forms.TextBox();
+            this.cbChildFolder = new System.Windows.Forms.CheckBox();
+            this.btnFindFile = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbEncode = new System.Windows.Forms.ComboBox();
+            this.btnChangeEncode = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rtbFileList = new System.Windows.Forms.RichTextBox();
+            this.labFileNum = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "更改编码的文件目录：";
+            // 
+            // tbFolder
+            // 
+            this.tbFolder.AllowDrop = true;
+            this.tbFolder.Location = new System.Drawing.Point(181, 19);
+            this.tbFolder.Name = "tbFolder";
+            this.tbFolder.Size = new System.Drawing.Size(338, 21);
+            this.tbFolder.TabIndex = 1;
+            this.tbFolder.DragDrop += new System.Windows.Forms.DragEventHandler(this.tbFolder_DragDrop);
+            this.tbFolder.DragEnter += new System.Windows.Forms.DragEventHandler(this.tbFolder_DragEnter);
+            this.tbFolder.Enter += new System.EventHandler(this.tbFolder_Enter);
+            this.tbFolder.Leave += new System.EventHandler(this.tbFolder_Leave);
+            // 
+            // btnSelevtFolder
+            // 
+            this.btnSelevtFolder.Location = new System.Drawing.Point(542, 17);
+            this.btnSelevtFolder.Name = "btnSelevtFolder";
+            this.btnSelevtFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnSelevtFolder.TabIndex = 2;
+            this.btnSelevtFolder.Text = "选择目录";
+            this.btnSelevtFolder.UseVisualStyleBackColor = true;
+            this.btnSelevtFolder.Click += new System.EventHandler(this.btnSelevtFolder_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "需要更改的文件的后缀名：";
+            // 
+            // tbsuffix
+            // 
+            this.tbsuffix.Location = new System.Drawing.Point(181, 56);
+            this.tbsuffix.Name = "tbsuffix";
+            this.tbsuffix.Size = new System.Drawing.Size(210, 21);
+            this.tbsuffix.TabIndex = 4;
+            this.tbsuffix.Enter += new System.EventHandler(this.txsuffix_Enter);
+            this.tbsuffix.Leave += new System.EventHandler(this.txsuffix_Leave);
+            // 
+            // cbChildFolder
+            // 
+            this.cbChildFolder.AutoSize = true;
+            this.cbChildFolder.Checked = true;
+            this.cbChildFolder.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbChildFolder.Location = new System.Drawing.Point(435, 56);
+            this.cbChildFolder.Name = "cbChildFolder";
+            this.cbChildFolder.Size = new System.Drawing.Size(84, 16);
+            this.cbChildFolder.TabIndex = 5;
+            this.cbChildFolder.Text = "包含子目录";
+            this.cbChildFolder.UseVisualStyleBackColor = true;
+            // 
+            // btnFindFile
+            // 
+            this.btnFindFile.Location = new System.Drawing.Point(542, 55);
+            this.btnFindFile.Name = "btnFindFile";
+            this.btnFindFile.Size = new System.Drawing.Size(75, 23);
+            this.btnFindFile.TabIndex = 6;
+            this.btnFindFile.Text = "查找文件";
+            this.btnFindFile.UseVisualStyleBackColor = true;
+            this.btnFindFile.Click += new System.EventHandler(this.btnFindFile_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "更改后的文件编码：";
+            // 
+            // cbEncode
+            // 
+            this.cbEncode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbEncode.FormattingEnabled = true;
+            this.cbEncode.Items.AddRange(new object[] {
+            "UTF-8",
+            "Unicode",
+            "ASCII",
+            "BigEndianUnicode",
+            "UTF32",
+            "UTF7",
+            "Default"});
+            this.cbEncode.Location = new System.Drawing.Point(181, 93);
+            this.cbEncode.Name = "cbEncode";
+            this.cbEncode.Size = new System.Drawing.Size(121, 20);
+            this.cbEncode.TabIndex = 8;
+            // 
+            // btnChangeEncode
+            // 
+            this.btnChangeEncode.Location = new System.Drawing.Point(542, 89);
+            this.btnChangeEncode.Name = "btnChangeEncode";
+            this.btnChangeEncode.Size = new System.Drawing.Size(75, 23);
+            this.btnChangeEncode.TabIndex = 9;
+            this.btnChangeEncode.Text = "更改编码";
+            this.btnChangeEncode.UseVisualStyleBackColor = true;
+            this.btnChangeEncode.Click += new System.EventHandler(this.btnChangeEncode_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.rtbFileList);
+            this.groupBox1.Location = new System.Drawing.Point(36, 157);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(693, 269);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "更改文件列表";
+            // 
+            // rtbFileList
+            // 
+            this.rtbFileList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbFileList.BackColor = System.Drawing.Color.White;
+            this.rtbFileList.Location = new System.Drawing.Point(6, 20);
+            this.rtbFileList.Name = "rtbFileList";
+            this.rtbFileList.ReadOnly = true;
+            this.rtbFileList.Size = new System.Drawing.Size(681, 243);
+            this.rtbFileList.TabIndex = 0;
+            this.rtbFileList.Text = "";
+            // 
+            // labFileNum
+            // 
+            this.labFileNum.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labFileNum.AutoSize = true;
+            this.labFileNum.Location = new System.Drawing.Point(626, 441);
+            this.labFileNum.Name = "labFileNum";
+            this.labFileNum.Size = new System.Drawing.Size(71, 12);
+            this.labFileNum.TabIndex = 11;
+            this.labFileNum.Text = "文件个数：0";
+            this.labFileNum.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 462);
+            this.Controls.Add(this.labFileNum);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnChangeEncode);
+            this.Controls.Add(this.cbEncode);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnFindFile);
+            this.Controls.Add(this.cbChildFolder);
+            this.Controls.Add(this.tbsuffix);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSelevtFolder);
+            this.Controls.Add(this.tbFolder);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbFolder;
+        private System.Windows.Forms.Button btnSelevtFolder;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbsuffix;
+        private System.Windows.Forms.CheckBox cbChildFolder;
+        private System.Windows.Forms.Button btnFindFile;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbEncode;
+        private System.Windows.Forms.Button btnChangeEncode;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox rtbFileList;
+        private System.Windows.Forms.Label labFileNum;
+    }
+}
+
---
+++ CodeModify/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodeModify
+{
+    public partial class Form1 : Form
+    {
+        private const string descSuffix = "输入修改的后缀，多个之间使用(,)隔开";
+        private const string descFolder = "可将文件夹拖动到输入框";
+
+        private List<string> suffixList=new List<string>(); 
+
+        private List<FileInfo> fileInfoList=new List<FileInfo>(); 
+
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private void Init()
+        {
+            TextBoxLostFocus(tbsuffix, descSuffix);
+            TextBoxLostFocus(tbFolder, descFolder);
+            cbEncode.SelectedIndex = 0;
+        }
+        /// <summary>
+        /// 后缀输入框获得焦点
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txsuffix_Enter(object sender, EventArgs e)
+        {
+            if (tbsuffix.Text == descSuffix)
+            {
+                TextBoxGetFocus(tbsuffix, "");
+            }
+        }
+        /// <summary>
+        /// 后缀输入框失去焦点
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txsuffix_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbsuffix.Text))
+            {
+                TextBoxLostFocus(tbsuffix, descSuffix);
+            }
+        }
+
+        private void TextBoxGetFocus(TextBox tb,string desc)
+        {
+            tb.ForeColor = ColorTranslator.FromHtml("#333333");
+            tb.Text = desc;
+        }
+        private void TextBoxLostFocus(TextBox tb, string desc)
+        {
+            tb.ForeColor = ColorTranslator.FromHtml("#999999");
+            tb.Text = desc;
+        }
+
+        private void btnSelevtFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                tbFolder.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void btnFindFile_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbFolder.Text))
+            {
+                MessageBox.Show("请选择目录！");
+                return;
+            }
+            if (tbsuffix.Text==descSuffix)
+            {
+                MessageBox.Show("请输入需要修改的文件的后缀！");
+                return;
+            }
+            suffixList.Clear();
+            fileInfoList.Clear();
+            rtbFileList.Text = "";
+            labFileNum.Text = "文件个数：0";
+            string strSuffix = tbsuffix.Text;
+            string[] suffixs = strSuffix.Split(',');
+            if (suffixs.Length>0)
+            {
+                for (int i = 0; i < suffixs.Length; i++)
+                {
+                    suffixList.Add(suffixs[i]);
+                }
+            }
+            DirectoryInfo di = new DirectoryInfo(tbFolder.Text);
+            if (!di.Exists)
+            {
+                MessageBox.Show(tbFolder.Text + "不是一个有效的路径！");
+            }
+            else
+            {
+                LoadFile(di);
+            }
+            if (fileInfoList.Count>0)
+            {
+                for (int i = 0; i < fileInfoList.Count; i++)
+                {
+                    rtbFileList.AppendText(fileInfoList[i].Name);
+                    rtbFileList.AppendText("\r\n");
+                }
+            }
+            labFileNum.Text = "文件个数："+fileInfoList.Count;
+        }
+
+        private void LoadFile(DirectoryInfo di)
+        {
+            DirectoryInfo[] dis = di.GetDirectories();
+            FileInfo[] fis = di.GetFiles();
+            if (fis.Length>0)
+            {
+                for (int i = 0; i < fis.Length; i++)
+                {
+                    string suff = fis[i].Extension;
+                    if (suff.Length > 0)//过滤掉没有后缀的
+                    {
+                        suff = suff.Remove(0, 1); //移除"."
+                        if (suffixList.Contains(suff))
+                        {
+                            fileInfoList.Add(fis[i]);
+                        }
+                    }
+                }
+            }
+            if (cbChildFolder.Checked)
+            {
+                if (dis.Length>0)
+                {
+                    for (int i = 0; i < dis.Length; i++)
+                    {
+                        DirectoryInfo di0 = new DirectoryInfo(dis[i].FullName);
+                        LoadFile(di0);
+                    }
+                }
+            }
+        }
+
+        private void btnChangeEncode_Click(object sender, EventArgs e)
+        {
+            if (fileInfoList.Count==0)
+            {
+                MessageBox.Show("请先选择文件！");
+                return;
+            }
+            string strcode = cbEncode.SelectedItem.ToString();
+            Encoding encode=Encoding.UTF8;
+            switch (strcode)
+            {
+                case "UTF-8":
+                    encode=Encoding.UTF8;
+                    break;
+                case "Unicode":
+                    encode = Encoding.Unicode;
+                    break;
+                case "ASCII":
+                    encode = Encoding.ASCII;
+                    break;
+                case "BigEndianUnicode":
+                    encode = Encoding.BigEndianUnicode;
+                    break;
+                case "UTF32":
+                    encode = Encoding.UTF32;
+                    break;
+                case "UTF7":
+                    encode = Encoding.UTF7;
+                    break;
+                case "Default":
+                    encode = Encoding.Default;
+                    break;
+            }
+            for (int i = 0; i < fileInfoList.Count; i++)
+            {
+                File.WriteAllText(fileInfoList[i].FullName, File.ReadAllText(fileInfoList[i].FullName, Encoding.Default), encode);
+            }
+            MessageBox.Show("文件修改完成！");
+        }
+
+        private void tbFolder_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Link;
+                this.tbFolder.Cursor = System.Windows.Forms.Cursors.Arrow;  //指定鼠标形状（更好看）
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+            
+        }
+
+        private void tbFolder_DragDrop(object sender, DragEventArgs e)
+        {
+            string path = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            tbFolder.Text = path;
+            this.tbFolder.Cursor = System.Windows.Forms.Cursors.IBeam; //还原鼠标形状
+        }
+
+        private void tbFolder_Enter(object sender, EventArgs e)
+        {
+            if (tbFolder.Text == descFolder)
+            {
+                TextBoxGetFocus(tbFolder, "");
+            }
+        }
+
+        private void tbFolder_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbFolder.Text))
+            {
+                TextBoxLostFocus(tbFolder, descFolder);
+            }
+        }
+    }
+}
